a7c7b4f4d538dae640f6d9dbf35ae199b63ee87a
sugama <sugama@www.jamadam.com>
2011-03-31 17:24:37 +0900

    written

--- lib/MojoX/Util/BodyFilter.pm
+++ lib/MojoX/Util/BodyFilter.pm
@@ -1,16 +1,46 @@
 package MojoX::Util::BodyFilter;
-
 use strict;
 use warnings;
-
-our $VERSION = '0.01';
-
-sub new {
-    my $class = shift;
-    my $self  = bless {}, $class;
-
-    $self;
-}
+use Mojo::Server::PSGI;
+	
+	sub set_mw {
+		
+		my $class = shift;
+		my ($app, $mws) = @_;
+		$app->hook(after_dispatch => sub {
+			my $self = shift;
+			my $res = _generate_psgi_res($self->res);
+			for my $e (@$mws) {
+				eval {
+					require File::Spec->catdir(split(/::/, $e)). '.pm';
+				};
+				if (! $@) {
+					my $mw = $e->new;
+					$mw->app(sub {$res});
+					$res = $mw->call;
+				}
+			}
+			$self->res->body(join '', $res->[2]->getline);
+		});
+	}
+	
+	sub _generate_psgi_res {
+		
+		my $res = shift;
+		
+		my $status = $res->code;
+		$res->fix_headers;
+		my $headers = $res->content->headers;
+		my @headers;
+		for my $name (@{$headers->names}) {
+			for my $values ($headers->header($name)) {
+				push @headers, $name => $_ for @$values;
+			}
+		}
+		
+		my $body = Mojo::Server::PSGI::_Handle->new(_res => $res);
+		return [$status, \@headers, $body];
+	}
 
 1;
 
@@ -18,18 +48,56 @@ __END__
 
 =head1 NAME
 
-MojoX::Util::BodyFilter - 
+MojoX::Util::BodyFilter - BodyFilter in Plack::Middleware style [EXPERIMENTAL]
 
 =head1 SYNOPSIS
 
-    use MojoX::Util::BodyFilter;
-    MojoX::Util::BodyFilter->new;
+    sub startup {
+        ....
+
+        use MojoX::Util::BodyFilter;
+		MojoX::Util::BodyFilter->set_mw($self, ['Some::MW1','Some::MW2'])
+    }
+	
+	package Some::MW1;
+	use strict;
+	use warnings;
+	use base qw( Plack::Middleware );
+	
+	sub call {
+		
+		my $self = shift;
+		my $res = $self->app->(@_);
+		$self->response_cb($res, sub {
+			my $res = shift;
+			my $h = Plack::Util::headers($res->[1]);
+			if ($h->get('Content-Type') =~ 'text/html') {
+				return sub {
+					my $chunk = shift;
+					
+					### DO SOMETHING
+					
+					return $chunk;
+				};
+			}
+			$res;
+		});
+	}
 
 =head1 DESCRIPTION
 
+MojoX::Util::BodyFilter allows you to activate Plack::Middleware style body
+filters on after_dispatch hook.
+
 =head1 METHODS
 
-=head2 new
+=head2 set_mw
+
+	MojoX::Util::BodyFilter->set_mw($self, ['some::mw1','some::mw2'])
+
+=head1 SEE ALSO
+
+L<Text::PSTemplate>, L<MojoX::Renderer>
 
 =head1 AUTHOR
 
