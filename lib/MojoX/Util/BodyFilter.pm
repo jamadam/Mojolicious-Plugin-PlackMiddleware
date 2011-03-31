8522b748eb0b5a09d9e2fd694e1167ed4619a51a
sugama <sugama@www.jamadam.com>
2011-03-31 17:21:53 +0900

    initial commit

---
+++ lib/MojoX/Util/BodyFilter.pm
@@ -0,0 +1,45 @@
+package MojoX::Util::BodyFilter;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    my $self  = bless {}, $class;
+
+    $self;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+MojoX::Util::BodyFilter - 
+
+=head1 SYNOPSIS
+
+    use MojoX::Util::BodyFilter;
+    MojoX::Util::BodyFilter->new;
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 new
+
+=head1 AUTHOR
+
+Sugama Keita, E<lt>sugama@jamadam.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Sugama Keita.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
