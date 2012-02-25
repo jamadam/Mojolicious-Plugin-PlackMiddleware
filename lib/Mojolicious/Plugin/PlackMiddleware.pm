144efeb5bc9151cb4de9274630c2c08efe83f02f
sugama <sugama@sv02.cutout.biz>
2012-02-25 09:55:56 +0900

    version++

--- lib/Mojolicious/Plugin/PlackMiddleware.pm
+++ lib/Mojolicious/Plugin/PlackMiddleware.pm
@@ -5,7 +5,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Plack::Util;
 use Mojo::Message::Request;
 use Mojo::Message::Response;
-our $VERSION = '0.23';
+our $VERSION = '0.24';
     
     ### ---
     ### register
