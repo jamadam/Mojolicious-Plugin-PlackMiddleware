8c5e338602c25ca8d93aae4797d9076e7cf9a722
sugama <k-sugama@skyarc.co.jp>
2014-05-29 14:38:37 +0900

    version++

--- lib/Mojolicious/Plugin/PlackMiddleware.pm
+++ lib/Mojolicious/Plugin/PlackMiddleware.pm
@@ -5,7 +5,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Plack::Util;
 use Mojo::Message::Request;
 use Mojo::Message::Response;
-our $VERSION = '0.29';
+our $VERSION = '0.30';
 use Scalar::Util 'weaken';
     
     ### ---
