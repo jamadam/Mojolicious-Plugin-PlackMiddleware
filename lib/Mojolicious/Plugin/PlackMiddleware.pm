ea53b32202a04e0448e9400719e16c737462d187
jamadam <sugama@jamadam.com>
2014-06-20 00:11:10 +0900

    set cpan no_index

--- lib/Mojolicious/Plugin/PlackMiddleware.pm
+++ lib/Mojolicious/Plugin/PlackMiddleware.pm
@@ -5,7 +5,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Plack::Util;
 use Mojo::Message::Request;
 use Mojo::Message::Response;
-our $VERSION = '0.31';
+our $VERSION = '0.32';
 use Scalar::Util 'weaken';
     
     ### ---
