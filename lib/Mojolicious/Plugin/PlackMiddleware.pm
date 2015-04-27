b876881cc9974b31d82d31123610d199d5029407
Keita Sugama <sugama@jamadam.com>
2015-04-28 01:16:48 +0900

    bump version

--- lib/Mojolicious/Plugin/PlackMiddleware.pm
+++ lib/Mojolicious/Plugin/PlackMiddleware.pm
@@ -5,7 +5,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Plack::Util;
 use Mojo::Message::Request;
 use Mojo::Message::Response;
-our $VERSION = '0.33';
+our $VERSION = '0.34';
 use Scalar::Util 'weaken';
     
     ### ---
